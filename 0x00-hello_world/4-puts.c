8cb5350f1bc9ac849f15371caabb913919c1f186
Stephani6G <stephaniegloria283@gmail.com>
2023-08-10 15:35:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,10 @@
 /**
  *main - print thestring inthe put function
  *Description :usimng the main function
- *this program prints  "Programming is like building a 
+ *this program prints Programming is like building a 
  *return:0 always
  *return -main 0 always
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
