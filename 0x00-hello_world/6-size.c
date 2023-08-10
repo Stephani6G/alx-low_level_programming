a7459051fcd360b60d5b25556786342f96303d3f
Stephani6G <stephaniegloria283@gmail.com>
2023-08-10 15:51:18 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-  *main -entry to code
-  *description :prints various types of sze
-  *return :0 always
-  */
+ * main - Entry point of function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int a;
@@ -13,9 +13,10 @@ int main(void)
 	char d;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d);
-	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(a);
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b);
- 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d);	
-
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
