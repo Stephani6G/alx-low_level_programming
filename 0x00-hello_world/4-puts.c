8be0585356d624ff4688015754e1a8a4c7322e4b
Stephani6G <stephaniegloria283@gmail.com>
2023-08-10 15:33:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  *main - print thestring inthe put function
- *
  *Description :usimng the main function
  *this program prints  "Programming is like building a 
  *return:0 always
+ *return -main 0 always
  */
 
 int main(void)
