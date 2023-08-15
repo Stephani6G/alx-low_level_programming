12f1422bfde8a293d084eec0773a923cac95c4aa
Stephani6G <stephaniegloria283@gmail.com>
2023-08-15 11:27:18 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,12 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (j =1; i<= 10; i++)
+	for (j = 1; i <= 10; i++)
 	{
-		for (j =97; j <=122; j++)
+		for (j = 97; j <= 122; j++)
 		{
 		_putchar(j);
 		}
+		_putchar('\n');
 	}
 }
