2f465cb716f68c88f4065d80120cdd5418ed2fa2
Stephani6G <stephaniegloria283@gmail.com>
2023-08-15 10:23:22 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,14 @@
  *description -prints alphabet in lowercase
  *Return:always 0
  */
+
 void print_alphabet(void)
 {
 	int i;
 
 	for (i = 97; i <= 122; i++)
 	{
-		_putchar(1);
+		_putchar(i);
 	}
 	_putchar('\n');
 }
