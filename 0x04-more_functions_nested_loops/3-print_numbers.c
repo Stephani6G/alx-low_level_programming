6681df46626773dd6f484bd11ed36cc902afaf78
Stephani6G <stephaniegloria283@gmail.com>
2023-08-17 16:45:41 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48 ; c <= 57 ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
