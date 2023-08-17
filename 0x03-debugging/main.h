3e970ded1ae57406ac57e55dfff5fa5ae2088ec1
Stephani6G <stephaniegloria283@gmail.com>
2023-08-17 15:46:07 -0700

    1-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,16 @@
 
 #include <stdio.h>
 
+/*Function that give the sign of any number*/
 void positive_or_negative(int i);
+
+/*Function that give the largest number of three*/
+int largest_number(int a, int b, int c);
+
+/*Function that convert day*/
+int convert_day(int month, int day);
+
+/*Function that print remaining*/
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
