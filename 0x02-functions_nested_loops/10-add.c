eeb8c58af3d46dcae0b0c473ac6f01b46f1ca57b
Stephani6G <stephaniegloria283@gmail.com>
2023-08-16 19:38:19 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,7 +3,9 @@
 
 /**
  * main - check the code
- *
+ * add - Adds two intergers
+ * @num1: The first interger to be added
+ * @num2: The second interger to be added
  * Return: Always 0.
  */
 int add(int num1, int num2)
