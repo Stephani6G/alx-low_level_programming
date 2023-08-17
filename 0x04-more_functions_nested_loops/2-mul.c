ac9c780249443ba39de5e9d7a8411ca0f85e662e
Stephani6G <stephaniegloria283@gmail.com>
2023-08-17 16:43:15 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Multiplies two integers ab
+ * @a: is integer 1.
+ * @b: is integer 2.
+ *
+ *Return: Returns the value of multiplication of ab
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
