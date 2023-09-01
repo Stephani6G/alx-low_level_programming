27e667efd5780b42fce3ec36b786db7991dbd09d
Stephani6G <stephaniegloria283@gmail.com>
2023-09-01 09:22:34 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc:number of command line argument
+ * @argv: name of array 
+ * return:0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,4 @@
+Arguments to main
+argc and argv
+What does argc and argv mean?
+how to compile with unused variables
