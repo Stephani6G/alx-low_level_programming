c973d9c446266cf5fd9aeae1ef5bc38ee577b14a
Stephani6G <stephaniegloria283@gmail.com>
2023-09-01 09:47:03 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,13 +8,9 @@
 *
 * Return: 0.
 */
-int main(int argc, char *argv[])
-{
-	int i;
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
 	return (0);
 }
