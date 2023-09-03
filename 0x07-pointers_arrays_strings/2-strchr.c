ca8db432a313943ae7cb1958df25effeaae5e6f7
Stephani6G <stephaniegloria283@gmail.com>
2023-09-03 06:26:46 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,13 @@ char *_strchr(char *s, char c)
 {
 	while (*s)
 	{
-		s++;
-		if (*s == c)
-		{
-		return (s);
-		}
+		if (*s != c)
+			s++;
+		else
+			return (s);
 	}
+	if (c == '\0')
+		return (s);
+
 	return (NULL);
 }
