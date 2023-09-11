cfa6ccda8497d906073710d75278da34ed72d503
Stephani6G <stephaniegloria283@gmail.com>
2023-09-11 13:55:38 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints the name of the file it was compiled from
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+
