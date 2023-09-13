cdd21bbb3448b67535187320214bdad12791727c
Stephani6G <stephaniegloria283@gmail.com>
2023-09-13 15:01:54 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name -a function that prints name
+ * @ name: name to be printed by function
+ * @f: function to send the name to
+ * Return: 0 Success
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
