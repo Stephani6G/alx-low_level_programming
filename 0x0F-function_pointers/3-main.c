bcf78a4963fbe13ab8e7dd6343a6471403620813
Stephani6G <stephaniegloria283@gmail.com>
2023-09-13 16:39:00 -0700

    3-get_op_func.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Prints the result of operations
+* @argc: The number of arguments
+* @argv: Argument vector
+* Return: 0 Success
+*/
+
+int main(int argc, char *argv[])
+{
+
+	int i, j, k;
+	int (*p)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
+
+	p = get_op_func(argv[2]);
+	if (p == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	k = (*p)(i, j);
+
+	printf("%d\n", k);
+	return (0);
+}
