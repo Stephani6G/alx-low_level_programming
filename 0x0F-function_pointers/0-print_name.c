8ae42f6db9f53588ba2ef70582eef51b8d249821
Stephani6G <stephaniegloria283@gmail.com>
2023-09-13 15:03:13 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 
 /**
  * print_name -a function that prints name
- * @ name: name to be printed by function
+ * @name: name to be printed by function
  * @f: function to send the name to
  * Return: 0 Success
  */
