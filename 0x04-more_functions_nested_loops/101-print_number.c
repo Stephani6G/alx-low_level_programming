ed5d01b87448dbdfb76e5dbc66825efbfd3a45bb
Stephani6G <stephaniegloria283@gmail.com>
2023-09-24 03:58:03 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,24 +1,50 @@
 #include "main.h"
 
-void print_prev_digits(int n);
-
 /**
 * print_number - prints an integer
 *
 * @n: number to print
 * Return: void
 */
+
 void print_number(int n)
 {
+	int ndigit = 0;
+	int N = n;
+	int i, j, Ldigit, next, K;
+
+	while (N != 0)
+	{
+		N = N / 10;
+		ndigit++;
+	}
+	N = n;
 	if (n < 0)
 	{
+		Ldigit = -(N + 10) % 10;
+		if (Ldigit < 0)
+		{
+			Ldigit = -N;
+		}
+		N = -(N + Ldigit) / 10;
 		_putchar('-');
 	}
-	if (n > 9 || n < -9)
-		print_prev_digits(n);
-	if (n < 0)
-		_putchar(((n % 10) * -1) + '0');
-	else
-		_putchar((n % 10) + '0');
-}
 
+	else
+	{
+		Ldigit = N % 10;
+		N = (N - Ldigit) / 10;
+	}
+
+	for (i = ndigit - 1; i >= 1; --i)
+	{
+		K = N;
+		for (j = (i - 1); j > 0; --j)
+		{
+			K = K / 10;
+		}
+		next = K % 10;
+		_putchar(next + '0');
+	}
+	_putchar(Ldigit + '0');
+}
