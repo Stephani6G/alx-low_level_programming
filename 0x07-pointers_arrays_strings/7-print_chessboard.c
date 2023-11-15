6aa6ce7ee285ed92fe98296a6b2841afa6548649
Stephani6G <stephaniegloria283@gmail.com>
2023-11-14 20:52:47 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_chessboard - afunction that prints a chessboar
+*
+* @a: Double pointer to the function
+*
+* Return: 0 success
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
