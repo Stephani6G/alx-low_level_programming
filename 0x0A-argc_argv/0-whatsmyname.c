285612f1df95528487c9cc501c7c48662a129ec1
Stephani6G <stephaniegloria283@gmail.com>
2023-11-16 21:35:38 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main -  program that prints its name, followed by a new line
+ * If you rename the program it will print the new name without recompile
+ * @argc: the number of command line arguments
+ * @argv: An array containing the program command line arguments
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0])
+
+	return (0);
+}
