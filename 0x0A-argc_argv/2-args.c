9f2123a57d0e66ee7c3ad2e61ebaac42d63d87ea
Stephani6G <stephaniegloria283@gmail.com>
2023-11-17 08:45:08 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints all arguments it receives
+ * @argc: number of cli arguments
+ * @argv: arrray name
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
