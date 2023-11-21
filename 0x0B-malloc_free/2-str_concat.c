5adb542e17a97f91f37cfb07adc3ceba96c6f082
Stephani6G <stephaniegloria283@gmail.com>
2023-11-20 21:08:56 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
-	if (S2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		;
@@ -36,6 +36,6 @@ char *str_concat(char *s1, char *s2)
 	stop = j;
 
 	for (j = 0; j <= stop; k++, j++)
-		s[k} = s2[j];
+		s[k] = s2[j];
 	return (s);
 }
