f93dd0246e861261d02e1a6e7074ec140256908b
Stephani6G <stephaniegloria283@gmail.com>
2023-11-29 00:23:07 -0800

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * print_address - a function that prints opcodes of main function
+  * @str: address passed-in
+  * @n: number of bytes to be printed
+  */
+
+void print_address(char *str, int n)
+{
+	int i = 0;
+
+	char ch;
+
+	while (i < n)
+	{
+		ch  = *(str + i);
+
+		printf("%.2hhx", ch);
+		if (i < (n - 1))
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+  * main - a function that prints its opcodes
+  * @argc: number of arguments to main
+  * @argv: string array of arguments to main
+  * Return: 0 always to terminate program
+  */
+
+int main(int argc, char *argv[])
+{
+	int nbytes;
+	char *s;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+
+		exit(1);
+
+	}
+	nbytes = atoi(argv[1]);
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	s = (char *)&main;
+
+	print_address(s, nbytes);
+
+	return (0);
+}
