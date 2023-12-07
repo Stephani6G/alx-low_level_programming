426f1a0a003b74cf3650a786584e9807afa60d54
Stephani6G <stephaniegloria283@gmail.com>
2023-12-07 11:03:30 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 void print_before_main(void);
 
 /**
  * print_message - Prints the specified message.
  */
-
 void print_message(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
