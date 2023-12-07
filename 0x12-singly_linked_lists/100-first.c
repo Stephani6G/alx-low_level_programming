a9656a9dce2bcfa58ca00665d1ccd4b602c96467
Stephani6G <stephaniegloria283@gmail.com>
2023-12-07 10:53:00 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void print_before_main(void);
+
+/**
+ * print_message - Prints the specified message.
+ */
+
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
