2e5568637f0cb26a64c3aad6200fe232c2a480f5
Stephani6G <stephaniegloria283@gmail.com>
2023-12-07 09:44:36 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,19 @@
  * Return: the number of nonde in the list_t
  */
 
-size_t print_list(const lists_t *h)
+size_t print_list(const list_t *h)
 {
-	size_t node = 0;
+	size_t s = 0;
 
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->length, h->str);
-		node++;
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		s++;
 	}
-	return (nodes);
+
+	return (s);
 }
