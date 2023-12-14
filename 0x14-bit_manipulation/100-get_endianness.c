c530522235dbf51ae04c6362ef0453b72a2e8854
Stephani6G <stephaniegloria283@gmail.com>
2023-12-13 20:23:27 -0800

    _putchar.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_endianness - function that  checks whether a machine
+ *	is big  or little
+ * Return: 1 if little endian, 0 otherwise
+ */
+
+int get_endianness(void)
+{
+	int test = 256;
+	void *i = &test;
+
+	if ((*((char *)i + 1) & 1) != 0)
+		return (1);
+	else
+		return (0);
+	return (1);
+}
