48292aa42d6d2fa1b8b2ded1ce0613d0426ebeca
Stephani6G <stephaniegloria283@gmail.com>
2023-12-18 10:52:38 -0800

    main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,23 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/*putchar*/
+int _putchar(char c);
+
+/* reads a text file and prints it to the POSIX standard output.*/
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/*function that creates a file.*/
+int create_file(const char *filename, char *text_content);
+
+int append_text_to_file(const char *filename, char *text_content);
+
+
+#endif /* _MAIN_H */
